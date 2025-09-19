0b542f81383c84dae2288fa6ca9b7708d40f4642
ArnaudJalbert <108292907+arnaud-pitchblack@users.noreply.github.com>
2025-09-19 14:22:17 -0400

    Add docstring to Location class for clarity on attributes and usage

--- apps/setlog/src/setlog/domain/entities/location.py
+++ apps/setlog/src/setlog/domain/entities/location.py
@@ -3,6 +3,17 @@ from dataclasses import dataclass
 
 @dataclass
 class Location:
+    """A Location represents a geographical place, typically used to denote the origin
+    of a musical act or event. It can also be associated with record labels or other entities
+    within the music industry.
+
+    We keep the location information simple with just continent, country, and city.
+
+    Attributes:
+        continent (str): The continent where the location is situated.
+        country (str): The country of the location.
+        city (str): The city of the location.
+    """
     continent: str
     country: str
     city: str
