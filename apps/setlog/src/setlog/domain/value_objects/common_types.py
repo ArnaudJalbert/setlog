cf9ee63ee49c1d9f3c865a060a9c45afbe781144
ArnaudJalbert <108292907+arnaud-pitchblack@users.noreply.github.com>
2025-09-19 14:32:41 -0400

    Change ID type from int to str for entity uniqueness

--- apps/setlog/src/setlog/domain/value_objects/common_types.py
+++ apps/setlog/src/setlog/domain/value_objects/common_types.py
@@ -1,5 +1,5 @@
 from typing import Annotated
 
-ID = Annotated[int, "Unique identifier for an entity"]
+ID = Annotated[str, "Unique identifier for an entity"]
 Name = Annotated[str, "Name of an entity"]
 Description = Annotated[str, "Description of an entity"]
